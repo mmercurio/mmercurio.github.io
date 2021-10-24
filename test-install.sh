e61a81f6e03282a97dfcfa0bab7e5a0afb000e7f
Michael Mercurio <michael@mmercurio.com>
2021-10-23 23:28:48 -0400

    more testing

--- test-install.sh
+++ test-install.sh
@@ -2,6 +2,17 @@
 
 set -e # -e: exit on error
 
-echo "Running install"
-echo "working directory is $(pwd)"
+# POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
+script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
+echo "Running install: $0"
+echo "working directory is $(pwd)"
+echo "script_dir $script_dir"
+
+echo "looking for test-install.sh..."
+if [ -f test-install.sh ]; then 
+  echo "test-install.sh exists in current directory."
+fi
+if [ -f "$script_dir/test-install.sh" ]; then
+  echo "test-install.sh exists in source_dir"
+fi
